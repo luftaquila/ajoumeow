b068b354ce7520caa49273476442c2b4c3c7aaca
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-01-03 21:12:17 +0900

    v3.6.0

--- Resources/google script.js
+++ Resources/google script.js
@@ -19,7 +19,7 @@ function handleResponse(e) {
       if(e.parameter['신청']) {
         for (i in headers) {
           if(i == 0) {
-            row.push(new Date());
+            row.push(Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"));
             continue;
           }
           row.push(e.parameter[headers[i]]);
@@ -28,15 +28,16 @@ function handleResponse(e) {
       }
       else if(e.parameter['수정']) {
         var data = sheet.getRange('B2:D').getValues();
-        for(var i = data.length - 1; i < 0; i--) {
-          if(e.parameter['날짜'] == data[i][1]) {
+        for(var i = data.length - 1; i >= 0; i--) {
+          if(JSON.stringify(new Date(new Date(e.parameter['날짜']) - 32400000)) == JSON.stringify(data[i][1])) {
             if(e.parameter['이름'] == data[i][0]) {
               if(e.parameter['코스'] == data[i][2]) {
-                sheet.getRange('B' + i + 2).setValue(e.parameter['수정 이름']);
-                sheet.getRange('C' + i + 2).setValue(e.parameter['수정 날짜']);
-                sheet.getRange('D' + i + 2).setValue(e.parameter['수정 코스']);
-                var log = [ new Date(), '수정', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'], e.parameter['수정 이름'], e.parameter['수정 날짜'], e.parameter['수정 코스'] ];
+                sheet.getRange('B' + (i + 2)).setValue(e.parameter['수정 이름']);
+                sheet.getRange('C' + (i + 2)).setValue(e.parameter['수정 날짜']);
+                sheet.getRange('D' + (i + 2)).setValue(e.parameter['수정 코스']);
+                var log = [ Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"), '수정', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'], e.parameter['수정 이름'], e.parameter['수정 날짜'], e.parameter['수정 코스'] ];
                 doc.getSheetByName('Edit Log').getRange(doc.getSheetByName('Edit Log').getLastRow() + 1, 1, 1, 8).setValues([log]);
+                doc.getSheetByName('Edit Log').getRange('A:H').setHorizontalAlignment('center');
                 break;
               } else continue;
             } else continue;
@@ -45,13 +46,14 @@ function handleResponse(e) {
       }
       else if(e.parameter['삭제']) {
         var data = sheet.getRange('B2:D').getValues();
-        for(var i = data.length - 1; i < 0; i--) {
-          if(e.parameter['날짜'] == data[i][1]) {
+        for(var i = data.length - 1; i >= 0; i--) {
+          if(JSON.stringify(new Date(new Date(e.parameter['날짜']) - 32400000)) == JSON.stringify(data[i][1])) {
             if(e.parameter['이름'] == data[i][0]) {
               if(e.parameter['코스'] == data[i][2]) {
                 sheet.deleteRow(i + 2);
-                var log = [ new Date(), '삭제', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'] ];
+                var log = [ Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"), '삭제', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'] ];
                 doc.getSheetByName('Edit Log').getRange(doc.getSheetByName('Edit Log').getLastRow() + 1, 1, 1, 5).setValues([log]);
+                doc.getSheetByName('Edit Log').getRange('A:H').setHorizontalAlignment('center');
                 break;
               } else continue;
             } else continue;
