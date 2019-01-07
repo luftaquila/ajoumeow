3c161ab497f3058c2255c5faa32648a26b24ccfd
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-01-07 17:30:17 +0900

    v3.7.3.3

--- Resources/google script.js
+++ Resources/google script.js
@@ -26,7 +26,7 @@ function handleResponse(e) {
       if(e.parameter['신청']) {
         for (i in headers) {
           if(i == 0) {
-            row.push(Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"));
+            row.push(new Date());
             continue;
           }
           row.push(e.parameter[headers[i]]);
@@ -42,7 +42,7 @@ function handleResponse(e) {
                 sheet.getRange('B' + (i + 2)).setValue(e.parameter['수정 이름']);
                 sheet.getRange('C' + (i + 2)).setValue(e.parameter['수정 날짜']);
                 sheet.getRange('D' + (i + 2)).setValue(e.parameter['수정 코스']);
-                var log = [ Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"), '수정', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'], e.parameter['수정 이름'], e.parameter['수정 날짜'], e.parameter['수정 코스'] ];
+                var log = [ new Date(), '수정', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'], e.parameter['수정 이름'], e.parameter['수정 날짜'], e.parameter['수정 코스'] ];
                 doc.getSheetByName('Edit Log').getRange(doc.getSheetByName('Edit Log').getLastRow() + 1, 1, 1, 8).setValues([log]);
                 doc.getSheetByName('Edit Log').getRange('A:H').setHorizontalAlignment('center');
                 break;
@@ -58,7 +58,7 @@ function handleResponse(e) {
             if(e.parameter['이름'] == data[i][0]) {
               if(e.parameter['코스'] == data[i][2]) {
                 sheet.deleteRow(i + 2);
-                var log = [ Utilities.formatDate(new Date(), "GMT+09:00", "yyyy. MM. dd. a hh:mm:ss"), '삭제', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'] ];
+                var log = [ new Date(), '삭제', e.parameter['이름'], e.parameter['날짜'], e.parameter['코스'] ];
                 doc.getSheetByName('Edit Log').getRange(doc.getSheetByName('Edit Log').getLastRow() + 1, 1, 1, 5).setValues([log]);
                 doc.getSheetByName('Edit Log').getRange('A:H').setHorizontalAlignment('center');
                 break;
