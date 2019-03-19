2ad1e376e90e29adad5c33a4c2a291cdd9e9704a
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-03-19 19:04:51 +0900

    v5.1.5 Map Lazyload Updated

--- Resources/main.js
+++ Resources/main.js
@@ -1,7 +1,5 @@
 $(function() {
-  $('#courseImages').load('/ajoumeyoumeow/Resources/Htmls/courseImages.html');
-  $('#modals').load('/ajoumeyoumeow/Resources/Htmls/modals.html');
-  $("img.lazyload").lazyload();
+  lazyload();
   clickEventListener();
   if(new Date().getDay() == 0 || new Date().getDay() == 6) {
     $('ul.tabs li').removeClass('current');
@@ -126,15 +124,13 @@ function setData() {
   $("#latestUpdate").html("Latest Update : " + new Date().format("TT hh시 MM분 ss초"));
   if(new Date().getDay() == 0) $("#thisWeekEnd_2").css("backgroundColor", "greenyellow");
   if(new Date().getDay() == 6) $("#thisWeekEnd_1").css("backgroundColor", "greenyellow");
-  for(var i = 1; i <= 5; i++)
-  {
+  for(var i = 1; i <= 5; i++) {
     if(new Date().getDay() == i) $("#thisWeek_" + i).css("backgroundColor", "greenyellow");
     $("#thisWeek_" + i).css("color", "#000000");
     $("#thisWeek_" + i).html(new Date(year, 0, i + ((week - 1) * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
     $("#nextWeek_" + i).html(new Date(year, 0, i + (week * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
     $("#nextTwoWeek_" + i).html(new Date(year, 0, i + ((week + 1) * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
-    for(var j = 1; j <= 2; j++)
-    {
+    for(var j = 1; j <= 2; j++) {
       $("#thisWeek_d" + i + "_" + j).html(yourNameIs(0, i - 1, 1, j));
       $("#thisWeek_s" + i + "_" + j).html(yourNameIs(0, i - 1, 2, j));
       $("#thisWeek_t" + i + "_" + j).html(yourNameIs(0, i - 1, 3, j));
@@ -146,14 +142,12 @@ function setData() {
       $("#nextTwoWeek_t" + i + "_" + j).html(yourNameIs(2, i - 1, 3, j));
     }
   }
-  for(var i = 1; i <= 2; i++)
-  {
+  for(var i = 1; i <= 2; i++) {
     $("#thisWeekEnd_" + i).css("color", "#000000");
     $("#thisWeekEnd_" + i).html(new Date(year, 0, i + 5 + ((week - 1) * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
     $("#nextWeekEnd_" + i).html(new Date(year, 0, i + 5 + ((week + 0) * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
     $("#nextTwoWeekEnd_" + i).html(new Date(year, 0, i + 5 + ((week + 1) * 7) - new Date(year, 0, (week * 7)).getDay()).format("m/d(ddd)"));
-    for(var j = 1; j <= 2; j++)
-    {
+    for(var j = 1; j <= 2; j++) {
       $('#thisWeekEnd_d' + i + "_" + j).html(yourNameIs(0, i + 4, 1, j));
       $('#thisWeekEnd_s' + i + "_" + j).html(yourNameIs(0, i + 4, 2, j));
       $('#thisWeekEnd_t' + i + "_" + j).html(yourNameIs(0, i + 4, 3, j));
@@ -233,8 +227,7 @@ function yourNameIs(p1, p2, course, rank) {
     else if(rank == 2) {
       if(result == datum[i][1]) {
         if((course == 1) && (datum[i][2].includes("1코스"))) {
-          for(var j = 1; result == datum[i + j][1]; j++)
-          {
+          for(var j = 1; result == datum[i + j][1]; j++) {
             if((course == 1) && (datum[i + j][2].includes("1코스"))) {
               if(datum[i][0] == datum[i + j][0])
                 continue;
@@ -244,8 +237,7 @@ function yourNameIs(p1, p2, course, rank) {
           }
         }
         else if((course == 2) && (datum[i][2].includes("2코스"))) {
-          for(var j = 1; result == datum[i + j][1]; j++)
-          {
+          for(var j = 1; result == datum[i + j][1]; j++) {
             if((course == 2) && (datum[i + j][2].includes("2코스"))) {
               if(datum[i][0] == datum[i + j][0])
                 continue;
@@ -255,8 +247,7 @@ function yourNameIs(p1, p2, course, rank) {
           }
         }
         else if((course == 3) && (datum[i][2].includes("3코스"))) {
-          for(var j = 1; result == datum[i + j][1]; j++)
-          {
+          for(var j = 1; result == datum[i + j][1]; j++) {
             if((course == 3) && (datum[i + j][2].includes("3코스"))) {
               if(datum[i][0] == datum[i + j][0])
                 continue;
@@ -271,8 +262,18 @@ function yourNameIs(p1, p2, course, rank) {
   return " ";
 }
 function dataSize(s, b, i, c) { for(b = i = 0; c = s.charCodeAt(i++); b += c >> 11 ? 3 : c >> 7 ? 2 : 1); return b; }
-function popupBlocker() { console.log('click'); MicroModal.close('rankModal'); Cookies.set('popup', 'hidden', {expires : 3}); }
+function popupBlocker() { MicroModal.close('rankModal'); Cookies.set('popup', 'hidden', {expires : 3}); }
 function mileageDisplayer() { MicroModal.close('rankModal'); MicroModal.show('mileModal'); }
+function updateLogDisplayer() {
+  if($('#toggle').html() == '▼ 업데이트 로그 보기') {
+    $('#more').css('display', 'block');
+    $('#toggle').html('▲ 업데이트 로그 접기');
+  }
+  else {
+    $('#more').css('display', 'none');
+    $('#toggle').html('▼ 업데이트 로그 보기');
+  }
+}
 function clickEventListener() {
   $('.reload').click(function() { load(); });
   $('#onNoticeClick').click(function() { MicroModal.show('noticeModal'); });
@@ -296,16 +297,6 @@ function clickEventListener() {
   $('#modeDelete').click(function() {
     $('#editBox').css('display', 'none');
     $('#submit').val('삭제하기');
-  });
-  $('#toggle').click(function() {
-    if($('#toggle').html() == '▼ 업데이트 로그 보기') {
-      $('#more').css('display', 'block');
-      $('#toggle').html('▲ 업데이트 로그 접기');
-    }
-    else {
-      $('#more').css('display', 'none');
-      $('#toggle').html('▼ 업데이트 로그 보기');
-    }
   });
   $('#rainbowBlock').click(function() {
     if($('#rainbowBlock').prop('checked')) {
@@ -356,14 +347,14 @@ function loadWeather() {
 }
 var dateFormat = function () {
   var	token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
-      timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
-      timezoneClip = /[^-+\dA-Z]/g,
-      pad = function (val, len) {
-        val = String(val);
-        len = len || 2;
-        while (val.length < len) val = "0" + val;
-        return val;
-      };
+    timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+    timezoneClip = /[^-+\dA-Z]/g,
+    pad = function (val, len) {
+      val = String(val);
+      len = len || 2;
+      while (val.length < len) val = "0" + val;
+      return val;
+    };
   return function (date, mask, utc) {
     var dF = dateFormat;
     if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
