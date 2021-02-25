e76bc3928b3c8d0fcd393725c4452674f1de7507
LUFT-AQUILA <mail@luftaquila.io>
2021-02-25 22:21:52 +0900

    FIX: kakaoClient auto-verifying

--- server/api/settings.js
+++ server/api/settings.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dateformat from 'dateformat';
 import bodyParser from 'body-parser';
 
+import client from '../config/node-kakao'
 import util from '../controllers/util/util.js';
 import { Response, Log } from '../controllers/util/interface.js';
 
@@ -28,12 +29,10 @@ router.put('*', util.isAdmin, async (req, res) => {
     let result = await util.query(`UPDATE settings SET value='${req.body.data}' WHERE name='${req.url.substring(1)}'`);
     util.logger(new Log('info', req.remoteIP, req.originalPath, '설정값 수정', req.method, 200, req.body, result));
     res.status(200).json(new Response('success', null, result));
-    /*
     if(req.url.substring(1) == 'currentSemister') {
       const target = client.channelManager.map.get(process.env.noticeChannelId);
-      await target.sendText('시스템 현재 학기가 변경되었습니다!\n\n 기존 회원 분들은 사이트의 회원 등록 - 기존 회원 메뉴를 통해 새 학기 명단에 다시 등록해 주세요. 새 학기에도 화이팅!');
+      await target.sendText('시스템에서 현재 학기 설정이 변경되었습니다!\n\n 새 학기에도 미유미유와 함께하실 기존 회원 분들께서는 사이트에서 회원 등록 -> 기존 회원을 선택해 새 학기 명단에 등록해 주세요. 새 학기에도 화이팅!');
     }
-    */
   }
   catch(e) {
     util.logger(new Log('error', req.remoteIP, req.originalPath, '설정값 수정 오류', req.method, 500, req.body, e.stack));
