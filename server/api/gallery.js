a1c9ec405984425576bdec75d1b4d52058dec66a
LUFT-AQUILA <mail@luftaquila.io>
2021-05-22 11:49:43 +0900

    IMPROVE: photo sort query

--- server/api/gallery.js
+++ server/api/gallery.js
@@ -29,17 +29,32 @@ router.get('/tags', async (req, res) => {
 });
 
 router.get('/photo', async (req, res) => {
-  const row = { latest: 'timestamp', popular: 'likes' };
+  const row = {
+    latest: 'photo_id',
+    popular: 'likes'
+  };
+  
   try {
-    const result = await util.query(`SELECT * FROM gallery_photo ORDER BY ${row[req.query.sort]} DESC LIMIT 10 OFFSET ${req.query.offset};`);
-    for(const i in result) {
-      const tags = await util.query(`SELECT tag_name FROM gallery_photo_tag WHERE photo_id='${result[i].photo_id}';`);
-      result[i].tags = tags.map(x => x.tag_name);
+    if(req.query.type == 'gallery') {
+      const result = await util.query(`SELECT * FROM gallery_photo ORDER BY ${row[req.query.sort]} DESC LIMIT 10 OFFSET ${req.query.offset};`);
+      for(const i in result) {
+        const tags = await util.query(`SELECT tag_name FROM gallery_photo_tag WHERE photo_id='${result[i].photo_id}';`);
+        result[i].tags = tags.map(x => x.tag_name);
+      }
+      res.send(result);
     }
-    res.send(result);
+    else if(req.query.type == 'uploader') {
+      const result = await util.query(`SELECT * FROM gallery_uploader WHERE photo_count > 0 ORDER BY ${row[req.query.sort] ? `${row[req.query.sort]} DESC,` : '' } uploader_name ASC LIMIT 10 OFFSET ${req.query.offset};`);
+      res.send(result);
+    }
+    else if(req.query.type == 'cat') {
+      const result = await util.query(`SELECT * FROM gallery_tag WHERE photo_count > 0 ORDER BY ${row[req.query.sort] ? `${row[req.query.sort]} DESC,` : '' } tag_name ASC LIMIT 10 OFFSET ${req.query.offset};`);
+      res.send(result);
+    }
+    else res.status(404).send('fail');
   }
   catch(e) {
-  
+    
   }
 });
 
@@ -64,7 +79,7 @@ router.post('/photo', util.isLogin, upload.any(), async (req, res) => {
     await conn.query(`INSERT INTO gallery_uploader(uploader_id, uploader_name, photo_count, newest_photo_id) VALUES(${req.decoded.id}, '${uploader_name}', 1, '${req.files[0].filename}') ON DUPLICATE KEY UPDATE photo_count=photo_count+1, newest_photo_id='${req.files[0].filename}';`);
     
     await conn.commit();
-    res.json();
+    res.send('ok');
   }
   catch(e) {
     console.error(e);
@@ -74,8 +89,25 @@ router.post('/photo', util.isLogin, upload.any(), async (req, res) => {
 });
 
 router.post('/like', async (req, res) => {
-  console.log(req.body);
-  res.send('ok');
+  const conn = await pool.getConnection();
+  try {
+    await conn.beginTransaction();
+    
+    const uploader = await conn.query(`SELECT uploader_id FROM gallery_photo WHERE photo_id='${req.body.photo_id}';`);
+    const tags = await conn.query(`SELECT tag_name FROM gallery_photo_tag WHERE photo_id='${req.body.photo_id}';`);
+    await conn.query(`UPDATE gallery_photo SET likes=likes+1 WHERE photo_id='${req.body.photo_id}';`);
+    tags.forEach(async tag => await conn.query(`UPDATE gallery_tag SET likes=likes+1 WHERE tag_name='${tag.tag_name}';`));
+    await conn.query(`UPDATE gallery_uploader SET likes=likes+1 WHERE uploader_id=${uploader[0].uploader_id};`);
+    
+    await conn.commit();
+    
+    res.send('ok');
+  }
+  catch(e) {
+    console.error(e);
+    await conn.rollback();
+  }
+  finally { conn.release(); }
 });
 
 export default router
