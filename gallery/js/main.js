a3a0c23049a1c2e03539ea0b938d86cf96099d4f
LUFT-AQUILA <mail@luftaquila.io>
2021-06-01 22:30:03 +0900

    MERGE

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,4 +1,5 @@
-$(function(){
+$(function() {
+	if(Cookies.get('jwt')) $('#mypage').css('display', 'block');
 
 	'use strict';
 
@@ -45,7 +46,7 @@ $(function(){
 	      main.removeClass('mobile-open');
 	    }
     });
-    
+
 	};
 	toggleMenu();
 
@@ -54,7 +55,7 @@ $(function(){
 		jQuery('.probootstrap-animate').waypoint( function( direction ) {
 
 			if( direction === 'down' && !jQuery(this.element).hasClass('probootstrap-animated') ) {
-				
+
 				i++;
 
 				jQuery(this.element).addClass('item-animate');
@@ -76,9 +77,9 @@ $(function(){
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
+
 				}, 100);
-				
+
 			}
 
 		} , { offset: '95%' } );
@@ -87,7 +88,7 @@ $(function(){
 
 	if ($('.probootstrap-main').length > 0 ) {
 		$('.probootstrap-main').imagesLoaded( {
-		  
+
 		  },
 		  function() {
 		  	if ($('.card').length > 0 ) {
