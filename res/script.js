42c540d67db7d051effc29fc30f6cc9ae2ae3b24
LUFT-AQUILA <mail@luftaquila.io>
2021-09-23 02:31:01 +0900

    FIX: namecard

--- res/script.js
+++ res/script.js
@@ -110,7 +110,7 @@ function eventListener() {
 
     if(!$(this).hasClass('calendar-table__inactive')) {
       if(user && user.role != '회원') $('.namecard').not('.example').addClass('partner');
-      if(user && user.ID) $('.namecard[data-id=' + user.ID + ']').closest('div').children('span').children('span.namecard').addClass('partner');
+      if(user && user.ID) $('.namecard[data-id=' + user.ID + ']').closest('td').children('span.namecard').addClass('partner');
       for(let namecard of $('.namecard').not('.partner').not('.example')) $(namecard).text($(namecard).text()[0] + $(namecard).text().slice(1).replace(/./g , '○'));
     }
     else if(!user) {
